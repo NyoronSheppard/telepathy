00618ada09d307c6e0d591b84a41aca05dd8af65
coffeearmy <Alisblack@bitbucket.org>
2012-08-09 17:14:42 +0200

    inicio

---
+++ src/com/retroapp/telepathy/Highscore.java
@@ -0,0 +1,24 @@
+package com.retroapp.telepathy;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.app.NavUtils;
+
+public class Highscore extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_highscore);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_highscore, menu);
+        return true;
+    }
+
+    
+}
---
+++ src/com/retroapp/telepathy/Instructions.java
@@ -0,0 +1,24 @@
+package com.retroapp.telepathy;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.app.NavUtils;
+
+public class Instructions extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instructions);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_instructions, menu);
+        return true;
+    }
+
+    
+}
